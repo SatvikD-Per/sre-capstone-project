4bb77e947871be992e884bb1702e4e65f1d77a59
Satvik Kumar D <satvikkumar.damera@gmail.com>
2026-04-01 19:14:52 +0000

    Initial Flask app with health check

---
+++ app/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import random
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "SRE Capstone Running on GCP!"
+
+@app.route("/health")
+def health():
+    if random.randint(1, 5) == 1:
+        return "FAIL", 500
+    return "OK", 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
